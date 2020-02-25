83ee2bc02f6242487e813b0d04a2279e5fbc17e0
eshekak <olenev.biz@gmail.com>
2020-02-25 15:14:02 +0300

    feat/active-development: Deleted extra code

--- src/cljs/my_website/db.cljs
+++ src/cljs/my_website/db.cljs
@@ -1,5 +1,3 @@
 (ns my-website.db)
 
-(let [x 3] (+ (* x x)))
-
-(def default-db {:name "re-frame"})
+(def state (atom {}))
--- src/cljs/my_website/views.cljs
+++ src/cljs/my_website/views.cljs
@@ -1,30 +1,3 @@
-(ns my-website.views
-  (:require
-    [re-frame.core :as re-frame]
-    [my-website.routes :as routes]
-    [my-website.subs :as subs]))
+(ns my-website.views)
 
-;; --------------------
-(defn home-panel
-  []
-  (let [name (re-frame/subscribe [::subs/name])]
-    (fn []
-      [:div (str "Hello from " @name ". This is the Home Page :)")
-       [:div [:a {:href (routes/url :about)} "go to About Page"]]])))
-
-(defn about-panel
-  []
-  (fn []
-    [:div "This is the About Page."
-     [:div [:a {:href (routes/url ::subs/home)} "go to Home Page"]]]))
-
-;; --------------------
-(defmulti panels identity)
-(defmethod panels :home-panel [] [home-panel])
-(defmethod panels :about-panel [] [about-panel])
-(defmethod panels :default [] [:div])
-
-(defn main-panel
-  []
-  (let [active-panel (re-frame/subscribe [::subs/active-panel])]
-    (fn [] (panels @active-panel))))
+(defn main [] [:div "Hey, there!"])
